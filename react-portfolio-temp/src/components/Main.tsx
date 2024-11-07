28bf5d908b4b10c55ce9a9317da15b5c9be7fa4a
Kunal112003 <112599849+Kunal112003@users.noreply.github.com>
2024-11-06 18:37:12 -0700

    made changes to the timeline

--- react-portfolio-temp/src/components/Main.tsx
+++ react-portfolio-temp/src/components/Main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import '../assets/styles/Main.scss';
+import kunal from '../assets/images/kunal.jpg';
 
 function Main() {
 
@@ -9,19 +10,23 @@ function Main() {
     <div className="container">
       <div className="about-section">
         <div className="image-wrapper">
-          <img src="https://my-aws-assets.s3.us-west-2.amazonaws.com/portfolio-img/avatar_circle.jpeg" alt="Avatar" />
+          
+          <img src={kunal} alt="Avatar" style={{ width: '250px', height: '250px', borderRadius: '50%' }}/>
+
+
+          
         </div>
         <div className="content">
           <div className="social_icons">
-            <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-            <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+            <a href="https://github.com/Kunal112003" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+            <a href="https://www.linkedin.com/in/kunal-rao-0956b9262/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
           </div>
-          <h1>Yuji Sato</h1>
-          <p>Full Stack Engineer</p>
+          <h1>Kunal Rao</h1>
+          <p>Software Developer</p>
 
           <div className="mobile_social_icons">
-            <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-            <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+            <a href="https://github.com/Kunal112003" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+            <a href="https://www.linkedin.com/in/kunal-rao-0956b9262/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
           </div>
         </div>
       </div>
--- react-portfolio-temp/src/components/Timeline.tsx
+++ react-portfolio-temp/src/components/Timeline.tsx
@@ -16,41 +16,47 @@ function Timeline() {
             className="vertical-timeline-element--work"
             contentStyle={{ background: 'white', color: 'rgb(39, 40, 34)' }}
             contentArrowStyle={{ borderRight: '7px solid  white' }}
-            date="2022 - present"
-            iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
+            date="2023"
+            iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)', borderRadius: '50%' }}
             icon={<FontAwesomeIcon icon={faBriefcase} />}
+            // Make the edges rounded
+            
           >
-            <h3 className="vertical-timeline-element-title">Technology Consultant</h3>
-            <h4 className="vertical-timeline-element-subtitle">Dallas, TX</h4>
+            <h3 className="vertical-timeline-element-title" style={{ color: 'blue', fontSize: '24px' }}>Software Developer</h3>
+            {/* company name */}
+            <h4 className="vertical-timeline-element-subtitle">UofA BluePrints</h4>
+            <h4 className="vertical-timeline-element-subtitle">Edmonton, AB</h4>
             <p>
-              Full-stack Web Development, GenAI/LLM, Project Management, Business Development
+              Full-stack Web Development, Project Management, Agile Developement
             </p>
           </VerticalTimelineElement>
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
-            date="2020 - 2022"
+            date="2023 - 2024"
             iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
             icon={<FontAwesomeIcon icon={faBriefcase} />}
           >
-            <h3 className="vertical-timeline-element-title">Full Stack Engineer</h3>
-            <h4 className="vertical-timeline-element-subtitle">Laie, HI</h4>
+            <h3 className="vertical-timeline-element-title" style={{ color: 'blue', fontSize: '24px' }}>Software Engineer</h3>
+            <h4 className="vertical-timeline-element-subtitle">SPEAR</h4>
+            <h4 className="vertical-timeline-element-subtitle">Edmonton, AB</h4>
             <p>
-              Frontend Development, Backend Development, User Experience, Team Leading
+              Backend Development, Machine Learning, Computer Vision, Automation, AI
             </p>
           </VerticalTimelineElement>
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
-            date="2021 - 2021"
+            date="2025"
             iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
             icon={<FontAwesomeIcon icon={faBriefcase} />}
           >
-            <h3 className="vertical-timeline-element-title">Staff Engineer Intern</h3>
-            <h4 className="vertical-timeline-element-subtitle">Laie, HI</h4>
+            <h3 className="vertical-timeline-element-title" style={{ color: 'blue', fontSize: '24px' }}>Software Developer Co-op</h3>
+            <h4 className="vertical-timeline-element-subtitle">Royal Bank of Canada (RBC)</h4>
+            <h4 className="vertical-timeline-element-subtitle">Calgary, AB </h4>
             <p>
-              Full-stack Development, API Development, User Experience
+              Full-stack Development, CRM Development, Agile, Scrum, DevOps
             </p>
           </VerticalTimelineElement>
-          <VerticalTimelineElement
+          {/* <VerticalTimelineElement
             className="vertical-timeline-element--work"
             date="2020 - 2020"
             iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
@@ -61,7 +67,7 @@ function Timeline() {
             <p>
               Automation, Data Governance, Statistical Analysis
             </p>
-          </VerticalTimelineElement>
+          </VerticalTimelineElement> */}
         </VerticalTimeline>
       </div>
     </div>
